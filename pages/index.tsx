c079c41e25161fb22d6b2145c78dcf1932c84a94
lialila <uche0790@gmail.com>
2025-03-25 19:10:31 +0100

    Initial commit from Create Next App

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,73 @@
 import { ColorModeButton } from "@/components/ui/color-mode";
-import { Input, Flex, Box, Button } from "@chakra-ui/react";
-import { MagnifyingGlass } from "@phosphor-icons/react";
+import UserCard from "@/components/UserCard";
+import GitHubUser from "@/models/GitHubUser";
+import { Input, Flex, Box, Button, Text, Center } from "@chakra-ui/react";
+import { MagnifyingGlass, User } from "@phosphor-icons/react";
 import { useState } from "react";
+
+// TODO: define global font families
+// TODO: add button links for followers and following
+// TODO: add loading states
+// TODO: fix dark theme mode
+// TODO: implement pagination for the repos
+
+const userTest: GitHubUser = {
+  login: "katjakay",
+  id: 118436971,
+  node_id: "U_kgDOBw80aw",
+  avatar_url: "https://avatars.githubusercontent.com/u/118436971?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/katjakay",
+  html_url: "https://github.com/katjakay",
+  followers_url: "https://api.github.com/users/katjakay/followers",
+  following_url: "https://api.github.com/users/katjakay/following{/other_user}",
+  gists_url: "https://api.github.com/users/katjakay/gists{/gist_id}",
+  starred_url: "https://api.github.com/users/katjakay/starred{/owner}{/repo}",
+  subscriptions_url: "https://api.github.com/users/katjakay/subscriptions",
+  organizations_url: "https://api.github.com/users/katjakay/orgs",
+  repos_url: "https://api.github.com/users/katjakay/repos",
+  events_url: "https://api.github.com/users/katjakay/events{/privacy}",
+  received_events_url: "https://api.github.com/users/katjakay/received_events",
+  type: "User",
+  user_view_type: "public",
+  site_admin: false,
+  name: "Katja Kay",
+  company: null,
+  blog: "",
+  location: "Vienna, Austria ",
+  email: null,
+  hireable: null,
+  bio: "👩🏻‍💻 Full-Stack Developer with Graphic Design background 👁",
+  twitter_username: "Katjakay",
+  public_repos: 29,
+  public_gists: 0,
+  followers: 2,
+  following: 13,
+  created_at: new Date("2022-11-17T09:46:01Z"),
+  updated_at: new Date("2025-03-19T12:54:33Z"),
+};
 
 const Home = () => {
   const [search, setSearch] = useState("");
   const [user, setUser] = useState(null);
+  const [apiError, setApiError] = useState(false);
+
+  // Fetch user data from Github API on submit
   const onSubmit = () => {
     fetch(`https://api.github.com/users/${search}`)
-      .then((res) => res.json())
+      .then(async (res) => {
+        if (res.status === 403) {
+          setApiError(true);
+          return Promise.reject("API limit reached");
+        }
+        return res.json();
+      })
       .then((data) => {
+        console.log("user", data);
         setUser(data);
       })
-      .catch((err) => {
-        console.error(err);
+      .catch(async (err) => {
+        console.error("Error fetching user: ", err);
       });
   };
 
@@ -26,6 +80,8 @@ const Home = () => {
       <Flex flexDir="column" alignItems="center" height="60%">
         <Box my="auto" maxWidth="90">
           <h1 style={{ fontFamily: "MonoSpace" }}>devfinder</h1>
+
+          {/* Input field */}
           <Flex
             alignItems="center"
             gap="3"
@@ -40,22 +96,38 @@ const Home = () => {
             <Input
               placeholder="Search Github username"
               size="sm"
-              htmlSize={36}
-              width="auto"
+              htmlSize={26}
+              //  width="auto"
               variant="flushed"
               onChange={(e) => setSearch(e.target.value)}
-              onKeyDown={onSubmit}
+              onKeyDown={(e) => e.key === "Enter" && onSubmit()}
             />
             <Button size="2xs" onClick={onSubmit}>
               SEARCH
             </Button>
           </Flex>
-          {/* TODO: implement UserCard */}
-          {/* {user && <UserCard user={user} />} */}
-          {user && user.name}
+
+          {/* API error message */}
+          {apiError && (
+            <Text color="red.500" fontSize="small" fontFamily="MonoSpace">
+              API limit reached, please try again later
+            </Text>
+          )}
+
+          {/* User Card */}
+          {user && !apiError && <UserCard user={user} />}
         </Box>
       </Flex>
     </Box>
   );
 };
 export default Home;
+
+{
+  /*for testing purposes*/
+}
+{
+  /* <Box mt="8">
+            <UserCard user={userTest} />
+          </Box>  */
+}
